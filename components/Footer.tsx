ecf37395cbe2b3220f331b65b1f62d9c633a50b7
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-23 11:01:32 +0100

    finish footer and a tags

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,22 +4,65 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <div className="footer">
-            <a
-                href="https://www.gabrielcmoris.com"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{" "}
-                <span className="logo">
-                    <Image
-                        src="/bust.svg"
-                        alt="GabrielCMoris-logo"
-                        width={25}
-                        height={30}
-                    />
-                </span>{" "}
-                All rights reserved © 2021
-            </a>
+            <div className="follow">
+                <h4>Follow Me</h4>
+                <div className="icons-followme">
+                    <a
+                        href="https://www.youtube.com/channel/UCZBKPQ3iQUrwrxChV8AXFAg"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Image
+                            src="/youtube.svg"
+                            alt="youtube-logo"
+                            width={50}
+                            height={50}
+                        />
+                    </a>
+                    <a
+                        href="https://www.twitch.tv/piano_subito"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Image
+                            src="/twich.svg"
+                            alt="twitch-logo"
+                            width={50}
+                            height={50}
+                        />
+                    </a>
+                    <a
+                        href="https://www.reddit.com/user/bottom_of_the_key"
+                        target="_blank" rel="noreferrer"
+                    >
+                        <Image
+                            src="/reddit.svg"
+                            alt="reddit-logo"
+                            width={50}
+                            height={50}
+                        />
+                    </a>
+                </div>
+            </div>
+
+            <div className="copyright">
+                <a
+                    href="https://www.gabrielcmoris.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{" "}
+                    <span className="logo">
+                        <Image
+                            src="/bust.svg"
+                            alt="GabrielCMoris-logo"
+                            width={25}
+                            height={30}
+                        />
+                    </span>{" "}
+                    All rights reserved © 2022
+                </a>
+            </div>
         </div>
     );
 };
