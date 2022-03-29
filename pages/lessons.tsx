23f1c4708ecc26a2f0640496d03357cc0a7b520d
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-29 15:35:00 +0200

    add lessons component and delete useless scss

---
+++ pages/lessons.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type { NextPage } from "next";
+
+const Lessons: NextPage = () => {
+  return (
+    <div className='lessons'>
+
+    </div>
+  )
+}
+
+export default Lessons
